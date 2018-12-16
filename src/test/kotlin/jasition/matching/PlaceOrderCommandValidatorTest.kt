2361ca5501df4c2e1b3a97b04b4604a3fcdcb48d
jasition <jasition@gmail.com>
2018-12-16 16:10:33 +0000

    First test

--- src/test/kotlin/jasition/matching/PlaceOrderCommandValidatorTest.kt
+++ src/test/kotlin/jasition/matching/PlaceOrderCommandValidatorTest.kt
@@ -1,5 +1,8 @@
 package jasition.matching
 
+import arrow.core.getOrHandle
+import arrow.core.right
+import io.kotlintest.fail
 import jasition.matching.domain.book.Books
 import jasition.matching.domain.order.*
 import jasition.matching.domain.order.command.PlaceOrderCommand
@@ -9,6 +12,8 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import strikt.assertions.isGreaterThanOrEqualTo
 import strikt.assertions.isTrue
 import java.time.Instant
 
@@ -33,6 +38,19 @@ object PlaceOrderCommandValidatorTest : Spek({
             it("should place the order on the book") {
                 expectThat(event.isRight()).isTrue()
 
+                val event = event.getOrHandle { (d) -> fail("Expect OrderPlacedEvent") }
+
+                expectThat(event.requestId).isEqualTo(command.requestId)
+                expectThat(event.whoRequested).isEqualTo(command.whoRequested)
+                expectThat(event.bookId).isEqualTo(command.bookId)
+                expectThat(event.orderType).isEqualTo(command.orderType)
+                expectThat(event.side).isEqualTo(command.side)
+                expectThat(event.price).isEqualTo(command.price)
+                expectThat(event.availableSize).isEqualTo(command.size)
+                expectThat(event.tradedSize).isEqualTo(0)
+                expectThat(event.cancelledSize).isEqualTo(0)
+                expectThat(event.timeInForce).isEqualTo(command.timeInForce)
+                expectThat(event.whenHappened).isGreaterThanOrEqualTo(command.whenRequested)
             }
         }
     }
