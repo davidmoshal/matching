4daf33dd9f3b1ac9e469fd785b9444625f413b77
Chow <jasition@gmail.com>
2018-12-30 16:36:58 +0000

    Recovery test cases with BooksCreatedEvent as the first event

--- src/test/kotlin/jasition/matching/domain/scenario/recovery/RecoverBooksFromEventReplayOnlyScenarioTest.kt
+++ src/test/kotlin/jasition/matching/domain/scenario/recovery/RecoverBooksFromEventReplayOnlyScenarioTest.kt
@@ -6,36 +6,49 @@ import jasition.cqrs.Transaction
 import jasition.cqrs.recovery.replay
 import jasition.matching.domain.aBookId
 import jasition.matching.domain.aBooks
-import jasition.matching.domain.book.BookId
-import jasition.matching.domain.book.Books
+import jasition.matching.domain.book.TradingStatus
+import jasition.matching.domain.book.command.CreateBooksCommand
 import jasition.matching.domain.countEventsByClass
 import jasition.matching.domain.randomPlaceOrderCommand
 import kotlin.random.Random
 
 internal class `Recover books from replaying events only` : FeatureSpec({
     val bookId = aBookId()
-    val books = aBooks(bookId)
+    val initial = Transaction(aBooks(bookId))
+    val initialBooks = initial.aggregate
 
+    val booksCreatedEvent = CreateBooksCommand(
+        bookId = bookId,
+        defaultTradingStatus = TradingStatus.OPEN_FOR_TRADING
+    ).validate()
+
+    var latest = initial
+        .append(booksCreatedEvent)
+        .append(booksCreatedEvent.play(initialBooks))
+
+    val commandCount = Random.nextInt(50, 100)
+    for (i in 0 until commandCount) {
+        randomPlaceOrderCommand(bookId = bookId)
+            .validate(latest.aggregate)
+            .fold({ rejected ->
+                latest = latest.append(rejected).append(rejected.play(latest.aggregate))
+            }, { placed ->
+                latest = latest.append(placed).append(placed.play(latest.aggregate))
+            })
+    }
     feature("Recover from event re-playing") {
-        val commandCount = Random.nextInt(10, 50)
         scenario("Recover the books that was created by $commandCount PlaceOrderCommands with random values (Placed, Rejected and Trade)") {
-            var latest = Transaction<BookId, Books>(books)
-
-            for (i in 0 until commandCount) {
-                randomPlaceOrderCommand(bookId = bookId)
-                    .validate(latest.aggregate)
-                    .fold({ rejected ->
-                        latest = latest.append(rejected).append(rejected.play(latest.aggregate))
-                    }, { placed ->
-                        latest = latest.append(placed).append(placed.play(latest.aggregate))
-                    })
-            }
 
             val (aggregate, events) = latest
 
-            println("Books to recover: lastEventId=${aggregate.lastEventId}, eventCountByType=${countEventsByClass(events)}")
+            println(
+                "Books to recover: lastEventId=${aggregate.lastEventId}" +
+                        ", buyBookDepth=${aggregate.buyLimitBook.entries.size()}" +
+                        ", sellBookDepth=${aggregate.sellLimitBook.entries.size()}" +
+                        ", eventCountByType=${countEventsByClass(events)}"
+            )
 
-            replay(initial = books, events = events) shouldBe aggregate
+            replay(initial = initialBooks, events = events) shouldBe aggregate
         }
     }
 })
