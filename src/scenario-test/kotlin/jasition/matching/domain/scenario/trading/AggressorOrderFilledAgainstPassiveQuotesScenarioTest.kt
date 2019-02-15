28a28e875a82e50c0fcf2ab0343430dc852c9b6e
Chow <jasition@gmail.com>
2019-02-15 16:23:51 +0800

    Consolidate aggressor orders against quuotes test cases into one parameterised test class

--- src/scenario-test/kotlin/jasition/matching/domain/scenario/trading/AggressorOrderFilledAgainstPassiveQuotesScenarioTest.kt
+++ src/scenario-test/kotlin/jasition/matching/domain/scenario/trading/AggressorOrderFilledAgainstPassiveQuotesScenarioTest.kt
@@ -12,11 +12,10 @@ import jasition.cqrs.commitOrThrow
 import jasition.matching.domain.*
 import jasition.matching.domain.book.BookId
 import jasition.matching.domain.book.Books
-import jasition.matching.domain.book.entry.EntrySizes
+import jasition.matching.domain.book.entry.*
 import jasition.matching.domain.book.entry.EntryStatus.FILLED
 import jasition.matching.domain.book.entry.EntryStatus.PARTIAL_FILL
 import jasition.matching.domain.book.entry.EntryType.LIMIT
-import jasition.matching.domain.book.entry.Price
 import jasition.matching.domain.book.entry.Side.BUY
 import jasition.matching.domain.book.entry.Side.SELL
 import jasition.matching.domain.book.entry.TimeInForce.GOOD_TILL_CANCEL
@@ -24,65 +23,127 @@ import jasition.matching.domain.book.entry.TimeInForce.IMMEDIATE_OR_CANCEL
 import jasition.matching.domain.client.Client
 import jasition.matching.domain.trade.event.TradeEvent
 
-internal class `Aggressor order filled and passive quote filled` : StringSpec({
+internal class `Aggressor order filled against passive quotes` : StringSpec({
     val bookId = aBookId()
 
     forall(
-        // 1. Quote: bid size, bid price, offer size, offer price
-        // 2. Order: side, type, time in force, size, price
-        // 3. Trade: passive entry index (even = buy(0, 2), odd = sell(1, 3)), size, price,
-        //           aggressor status, aggressor available size, aggressor traded size
+        /**
+         * 1. Quote: bid size, bid price, offer size, offer price
+         * 2. Order: side, type, time in force, size, price
+         * 3. Trade: passive entry index (even = buy(0, 2), odd = sell(1, 3)), size, price,
+         *           aggressor status, aggressor available size, aggressor traded size,
+         *           passive status, passive available size
+         *
+         * Parameter dimensions
+         * 1. Buy / Sell of aggressor order
+         * 2. Fill / Partial-fill of passive quotes
+         * 3. GTC / IOC of aggressor order
+         * 4. Single / Multiple fills
+         * 5. Exact / Better price executions (embedded in multiple fill cases)
+         */
 
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(BUY, LIMIT, GOOD_TILL_CANCEL, 6, 12L),
-            List.of(Tuple6(1, 6, 12L, FILLED, 0, 6))
+            List.of(Tuple8(1, 6, 12L, FILLED, 0, 6, FILLED, 0))
         ),
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(BUY, LIMIT, IMMEDIATE_OR_CANCEL, 6, 12L),
-            List.of(Tuple6(1, 6, 12L, FILLED, 0, 6))
+            List.of(Tuple8(1, 6, 12L, FILLED, 0, 6, FILLED, 0))
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(BUY, LIMIT, GOOD_TILL_CANCEL, 2, 12L),
+            List.of(Tuple8(1, 2, 12L, FILLED, 0, 2, PARTIAL_FILL, 4))
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(BUY, LIMIT, IMMEDIATE_OR_CANCEL, 2, 12L),
+            List.of(Tuple8(1, 2, 12L, FILLED, 0, 2, PARTIAL_FILL, 4))
         ),
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(SELL, LIMIT, GOOD_TILL_CANCEL, 6, 11L),
-            List.of(Tuple6(0, 6, 11L, FILLED, 0, 6))
+            List.of(Tuple8(0, 6, 11L, FILLED, 0, 6, FILLED, 0))
         ),
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(SELL, LIMIT, IMMEDIATE_OR_CANCEL, 6, 11L),
-            List.of(Tuple6(0, 6, 11L, FILLED, 0, 6))
+            List.of(Tuple8(0, 6, 11L, FILLED, 0, 6, FILLED, 0))
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(SELL, LIMIT, GOOD_TILL_CANCEL, 4, 11L),
+            List.of(Tuple8(0, 4, 11L, FILLED, 0, 4, PARTIAL_FILL, 2))
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(SELL, LIMIT, IMMEDIATE_OR_CANCEL, 4, 11L),
+            List.of(Tuple8(0, 4, 11L, FILLED, 0, 4, PARTIAL_FILL, 2))
         ),
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(BUY, LIMIT, GOOD_TILL_CANCEL, 13, 13L),
             List.of(
-                Tuple6(1, 6, 12L, PARTIAL_FILL, 7, 6),
-                Tuple6(3, 7, 13L, FILLED, 0, 13)
+                Tuple8(1, 6, 12L, PARTIAL_FILL, 7, 6, FILLED, 0),
+                Tuple8(3, 7, 13L, FILLED, 0, 13, FILLED, 0)
             )
         ),
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(BUY, LIMIT, IMMEDIATE_OR_CANCEL, 13, 13L),
             List.of(
-                Tuple6(1, 6, 12L, PARTIAL_FILL, 7, 6),
-                Tuple6(3, 7, 13L, FILLED, 0, 13)
+                Tuple8(1, 6, 12L, PARTIAL_FILL, 7, 6, FILLED, 0),
+                Tuple8(3, 7, 13L, FILLED, 0, 13, FILLED, 0)
+            )
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(BUY, LIMIT, GOOD_TILL_CANCEL, 10, 13L),
+            List.of(
+                Tuple8(1, 6, 12L, PARTIAL_FILL, 4, 6, FILLED, 0),
+                Tuple8(3, 4, 13L, FILLED, 0, 10, PARTIAL_FILL, 3)
+            )
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(BUY, LIMIT, IMMEDIATE_OR_CANCEL, 10, 13L),
+            List.of(
+                Tuple8(1, 6, 12L, PARTIAL_FILL, 4, 6, FILLED, 0),
+                Tuple8(3, 4, 13L, FILLED, 0, 10, PARTIAL_FILL, 3)
             )
         ),
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(SELL, LIMIT, GOOD_TILL_CANCEL, 13, 10L),
             List.of(
-                Tuple6(0, 6, 11L, PARTIAL_FILL, 7, 6),
-                Tuple6(2, 7, 10L, FILLED, 0, 13)
+                Tuple8(0, 6, 11L, PARTIAL_FILL, 7, 6, FILLED, 0),
+                Tuple8(2, 7, 10L, FILLED, 0, 13, FILLED, 0)
             )
         ),
         row(
             List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
             Tuple5(SELL, LIMIT, IMMEDIATE_OR_CANCEL, 13, 10L),
             List.of(
-                Tuple6(0, 6, 11L, PARTIAL_FILL, 7, 6),
-                Tuple6(2, 7, 10L, FILLED, 0, 13)
+                Tuple8(0, 6, 11L, PARTIAL_FILL, 7, 6, FILLED, 0),
+                Tuple8(2, 7, 10L, FILLED, 0, 13, FILLED, 0)
+            )
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(SELL, LIMIT, GOOD_TILL_CANCEL, 10, 10L),
+            List.of(
+                Tuple8(0, 6, 11L, PARTIAL_FILL, 4, 6, FILLED, 0),
+                Tuple8(2, 4, 10L, FILLED, 0, 10, PARTIAL_FILL, 3)
+            )
+        ),
+        row(
+            List.of(Tuple4(6, 11L, 6, 12L), Tuple4(7, 10L, 7, 13L)),
+            Tuple5(SELL, LIMIT, IMMEDIATE_OR_CANCEL, 10, 10L),
+            List.of(
+                Tuple8(0, 6, 11L, PARTIAL_FILL, 4, 6, FILLED, 0),
+                Tuple8(2, 4, 10L, FILLED, 0, 10, PARTIAL_FILL, 3)
             )
         )
     ) { oldEntries, new, expectedTrades ->
@@ -102,7 +163,8 @@ internal class `Aggressor order filled and passive quote filled` : StringSpec({
                 entryType = new.b,
                 timeInForce = new.c,
                 size = new.d,
-                price = Price(new.e)
+                price = Price(new.e),
+                whoRequested = Client(firmId = "firm2", firmClientId = randomFirmClientId())
             )
 
             val result = command.execute(repo.read(bookId)) commitOrThrow repo
@@ -140,11 +202,11 @@ internal class `Aggressor order filled and passive quote filled` : StringSpec({
                         passive = expectedTradeSideEntry(
                             bookEntry = oldBookEntries[trade.a].copy(
                                 sizes = EntrySizes(
-                                    available = 0,
+                                    available = trade.h,
                                     traded = trade.b,
                                     cancelled = 0
                                 ),
-                                status = FILLED
+                                status = trade.g
                             )
                         )
                     )
@@ -157,11 +219,36 @@ internal class `Aggressor order filled and passive quote filled` : StringSpec({
                         entry.side == side
                     }
 
-                    side.oppositeSideBook(it).entries.values() shouldBe oldBookEntries.filter { entry ->
-                        entry.side == side.oppositeSide()
-                    }.removeAll(expectedTrades.map { trade -> oldBookEntries[trade.a] })
+                    side.oppositeSideBook(it).entries.values() shouldBe updatedBookEntries(
+                        side = side,
+                        oldBookEntries = oldBookEntries,
+                        expectedTrades = expectedTrades
+                    )
                 }
             }
         }
     }
 })
+
+fun updatedBookEntries(
+    side: Side,
+    oldBookEntries: List<BookEntry>,
+    expectedTrades: List<Tuple8<Int, Int, Long, EntryStatus, Int, Int, EntryStatus, Int>>
+): List<BookEntry> {
+    var updatedBookEntries = oldBookEntries
+    expectedTrades.forEach { t ->
+        updatedBookEntries = updatedBookEntries.update(t.a) { e ->
+            e.copy(
+                sizes = EntrySizes(
+                    available = t.h,
+                    traded = t.b,
+                    cancelled = 0
+                ),
+                status = t.g
+            )
+        }
+    }
+    return updatedBookEntries.filter { entry ->
+        entry.side == side.oppositeSide() && !entry.status.isFinal()
+    }
+}
