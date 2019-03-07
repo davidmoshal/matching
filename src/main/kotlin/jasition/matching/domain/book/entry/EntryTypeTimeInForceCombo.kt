168ed92aceaeb17e279ea7ac7f1477f3afa964df
Chow <jasition@gmail.com>
2019-03-07 18:48:12 +0000

    FOK unit tests

--- src/main/kotlin/jasition/matching/domain/book/entry/EntryTypeTimeInForceCombo.kt
+++ src/main/kotlin/jasition/matching/domain/book/entry/EntryTypeTimeInForceCombo.kt
@@ -4,13 +4,12 @@ import io.vavr.kotlin.linkedHashMap
 import io.vavr.kotlin.list
 import jasition.matching.domain.book.entry.EntryType.LIMIT
 import jasition.matching.domain.book.entry.EntryType.MARKET
-import jasition.matching.domain.book.entry.TimeInForce.GOOD_TILL_CANCEL
-import jasition.matching.domain.book.entry.TimeInForce.IMMEDIATE_OR_CANCEL
+import jasition.matching.domain.book.entry.TimeInForce.*
 
 object EntryTypeTimeInForceCombo {
     private val validCombos = linkedHashMap(
-        Pair(LIMIT, list(GOOD_TILL_CANCEL, IMMEDIATE_OR_CANCEL)),
-        Pair(MARKET, list(IMMEDIATE_OR_CANCEL))
+        Pair(LIMIT, list(GOOD_TILL_CANCEL, IMMEDIATE_OR_CANCEL, FILL_OR_KILL)),
+        Pair(MARKET, list(IMMEDIATE_OR_CANCEL, FILL_OR_KILL))
     )
 
     fun isValid(entryType: EntryType, timeInForce: TimeInForce): Boolean =
--- src/test/kotlin/jasition/matching/domain/book/entry/EntryTypeTimeInForceComboTest.kt
+++ src/test/kotlin/jasition/matching/domain/book/entry/EntryTypeTimeInForceComboTest.kt
@@ -6,16 +6,17 @@ import io.kotlintest.specs.StringSpec
 import io.kotlintest.tables.row
 import jasition.matching.domain.book.entry.EntryType.LIMIT
 import jasition.matching.domain.book.entry.EntryType.MARKET
-import jasition.matching.domain.book.entry.TimeInForce.GOOD_TILL_CANCEL
-import jasition.matching.domain.book.entry.TimeInForce.IMMEDIATE_OR_CANCEL
+import jasition.matching.domain.book.entry.TimeInForce.*
 
 
 internal class EntryTypeTimeInForceComboTest : StringSpec({
     forall(
         row(LIMIT, GOOD_TILL_CANCEL, true),
         row(LIMIT, IMMEDIATE_OR_CANCEL, true),
+        row(LIMIT, FILL_OR_KILL, true),
         row(MARKET, GOOD_TILL_CANCEL, false),
-        row(MARKET, IMMEDIATE_OR_CANCEL, true)
+        row(MARKET, IMMEDIATE_OR_CANCEL, true),
+        row(MARKET, FILL_OR_KILL, true)
     ) { entryType, timeInForce, expectedResult ->
         "$entryType and $timeInForce is ${if (expectedResult) "a valid" else "an invalid"} combo" {
             EntryTypeTimeInForceCombo.isValid(entryType, timeInForce) shouldBe expectedResult
